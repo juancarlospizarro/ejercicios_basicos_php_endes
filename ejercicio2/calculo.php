af0e10cf83853cb3720e717b8653eb5a54c1914b
Juan Carlos Pizarro Alonso <127835047+juancarlospizarro@users.noreply.github.com>
2024-10-10 10:13:15 +0200

    Add files via upload

--- ejercicio2/calculo.php
+++ ejercicio2/calculo.php
@@ -1,1 +1,10 @@
+<?php
+$velocidad = $_GET['velocidad'];
+$distancia = $_GET['distancia'];
+function tiempo($velocidad, $distancia) {
+    return $distancia / $velocidad;
+}
+echo "El tiempo del viaje es " .tiempo($velocidad, $distancia);
+?>
 
+<a href="calculo.html">Atrás</a>
